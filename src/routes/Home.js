ade4d9d9643d3ef397c7a34938a82c947e25501f
SWH <slgo123@naver.com>
2021-02-20 00:49:08 +0900

    ':tada:'

---
+++ src/routes/Home.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+class Home extends React.Component {
+  render() {
+    return <div> 로그인 후 페이지</div>
+  }
+}
+export default Home
---
+++ src/routes/Home_login.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "./Home_login.css"
+
+class HomeLogin extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <div className="main_body">
+          <div className="main_body_login">
+            <div className="main_body_login_box">로그인 입력창</div>
+          </div>
+        </div>
+        <div className="main_footer">
+          <div className="main_footer_inner">
+            소개 블로그 테스트 테스트 테스트 테스트 테스트 테스트 테스트
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default HomeLogin
