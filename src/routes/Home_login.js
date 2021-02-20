ec1a5565d40b7cfa104c4286621a2b94ffc96457
SWH <slgo123@naver.com>
2021-02-20 22:02:58 +0900

    '✨loginpage'

--- src/routes/Home_login.js
+++ src/routes/Home_login.js
@@ -7,7 +7,28 @@ class HomeLogin extends React.Component {
       <div className="main">
         <div className="main_body">
           <div className="main_body_login">
-            <div className="main_body_login_box">로그인 입력창</div>
+            <div className="main_body_login_wrap">
+              <div className="main_body_login_wrap_box">
+                <div className="main_body_login_wrap_box_logo"></div>
+                <div className="main_body_login_wrap_box_inputbox">
+                  <div className="main_body_login_wrap_box_input">
+                    <input type="text"></input>
+                  </div>
+                  <div className="main_body_login_wrap_box_input">
+                    <input type="password"></input>
+                  </div>
+                  <div className="main_body_login_wrap_box_loginbtn">
+                    로그인버튼
+                  </div>
+                  <div className="main_body_login_wrap_box_content">
+                    --- 또는 ---
+                  </div>
+                  <div className="main_body_login_wrap_box_google">
+                    구글 로그인
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="main_footer">
