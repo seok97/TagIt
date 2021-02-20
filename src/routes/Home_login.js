eb837058dbed718b8689c299e4d79cce1ec475d7
SWH <slgo123@naver.com>
2021-02-21 00:37:59 +0900

    'uiupdate'

--- src/routes/Home_login.js
+++ src/routes/Home_login.js
@@ -8,24 +8,42 @@ class HomeLogin extends React.Component {
         <div className="main_body">
           <div className="main_body_login">
             <div className="main_body_login_wrap">
-              <div className="main_body_login_wrap_box">
-                <div className="main_body_login_wrap_box_logo"></div>
-                <div className="main_body_login_wrap_box_inputbox">
-                  <div className="main_body_login_wrap_box_input">
-                    <input type="text"></input>
-                  </div>
-                  <div className="main_body_login_wrap_box_input">
-                    <input type="password"></input>
-                  </div>
-                  <div className="main_body_login_wrap_box_loginbtn">
-                    로그인버튼
-                  </div>
-                  <div className="main_body_login_wrap_box_content">
-                    --- 또는 ---
-                  </div>
-                  <div className="main_body_login_wrap_box_google">
-                    구글 로그인
-                  </div>
+              <div className="main_body_login_wrap_box_logo">
+                <h1 className="main_logo_h1">TagIt</h1>
+              </div>
+              <div className="main_body_login_wrap_box_inputbox">
+                <div className="main_body_login_wrap_box_input divfelxbox">
+                  <input
+                    id="user_id"
+                    className="main_login_input"
+                    type="text"></input>
+                  <label className="inputlabel" for="user_id">
+                    사용자 이메일
+                  </label>
+                </div>
+                <div className="main_body_login_wrap_box_input divfelxbox">
+                  <input
+                    id="user_pw"
+                    className="main_login_input"
+                    type="password"></input>
+                  <label className="inputlabel" for="user_pw">
+                    비밀번호
+                  </label>
+                </div>
+                <div className="main_body_login_wrap_box_loginbtn divfelxbox">
+                  <button className="loginbtn" type="submit">
+                    <div className="main_body_login_wrap_box_loginbtn_btn">
+                      로그인
+                    </div>
+                  </button>
+                </div>
+                <div className="main_body_login_wrap_box_content">
+                  <div className="logindeco"></div>
+                  <div className="logindecotxt">또는</div>
+                  <div className="logindeco"></div>
+                </div>
+                <div className="main_body_login_wrap_box_google">
+                  계정이 없으신가요? 가입하기
                 </div>
               </div>
             </div>
