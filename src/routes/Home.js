61908d62dd2d06bdc68d8de2e35006d906c76135
SWH <slgo123@naver.com>
2021-03-11 14:22:41 +0900

    'css'

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,8 +12,18 @@ function Home() {
       <div className="home_top">
         <div className="home_top_navi">
           <div className="home_top_navi_inner01">
-            <div className="home_top_navi_logowrap">
-              <h1 className="home_top_navi_logo">TagIt</h1>
+            <div className="home_top_navi_inner02">
+              <div className="home_top_navi_logowrap">
+                <h1 className="home_top_navi_logo">TagIt</h1>
+              </div>
+              <ul className="navi">
+                <li>로그아웃</li>
+                <li>마이페이지</li>
+                <li>탐색</li>
+                <li>
+                  <input type="text"></input>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
